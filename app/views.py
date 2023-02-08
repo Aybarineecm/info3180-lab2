b69d46d2b780228f8ee4c1dfb696bbee212de668
Aybarinee Campbell Mendez <Aybarineecampbellmendez@gmail.com>
2023-02-08 14:51:45 -0500

    added profile to nav bar

--- app/views.py
+++ app/views.py
@@ -16,7 +16,7 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Aybarinee Campbell Mendez")
 
 @app.route('/profile')
 def profile():
